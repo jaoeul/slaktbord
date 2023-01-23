7378f08deffa9f8b36ef3d3e9597d2e9598e5596
Joel <joel@tabbingto.space>
2023-01-23 18:49:05 +0100

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,133 @@
 
 use core::arch::asm;
 
+struct Pin {
+    addr: *const u8,
+    ddr: *const u8,
+    mask: u8,
+}
+
+impl Pin {
+    fn new(addr: u16, ddr: u16, mask: u8) -> Pin {
+        Pin {
+            addr: addr as *const u8,
+            ddr: ddr as *const u8,
+            mask: mask,
+        }
+    }
+
+    unsafe fn pin_on(&mut self) {
+        core::ptr::write_volatile(
+            self.addr as *mut u8, core::ptr::read_volatile(self.addr) | self.mask);
+    }
+
+    unsafe fn pin_off(&mut self) {
+        core::ptr::write_volatile(
+            self.addr as *mut u8, core::ptr::read_volatile(self.addr) & !self.mask);
+    }
+
+    unsafe fn set_out(&mut self) {
+        core::ptr::write_volatile(
+            self.ddr as *mut u8, core::ptr::read_volatile(self.ddr) | self.mask);
+    }
+
+    unsafe fn set_in(&mut self) {
+        core::ptr::write_volatile(
+            self.ddr as *mut u8, core::ptr::read_volatile(self.ddr) & !self.mask);
+    }
+}
+
+struct Atmega32p8 {
+    /// Port B
+    PB0: Pin,
+    PB1: Pin,
+    PB2: Pin,
+    PB3: Pin,
+    PB4: Pin,
+    PB5: Pin,
+    PB6: Pin,
+    PB7: Pin,
+
+    /// Port C
+    PC6: Pin,
+    PC7: Pin,
+
+    /// Port D
+    PD0: Pin,
+    PD1: Pin,
+    PD2: Pin,
+    PD3: Pin,
+    PD4: Pin,
+    PD5: Pin,
+    PD6: Pin,
+    PD7: Pin,
+
+    /// Port E
+    PE2: Pin,
+    PE6: Pin,
+
+    /// Port F
+    PF0: Pin,
+    PF1: Pin,
+    PF4: Pin,
+    PF5: Pin,
+    PF6: Pin,
+    PF7: Pin,
+}
+
+impl Atmega32p8 {
+    fn new() -> Atmega32p8 {
+        Atmega32p8 {
+            /// Port B
+            PB0: Pin::new(0x25, 0x24, 1 << 0),
+            PB1: Pin::new(0x25, 0x24, 1 << 1),
+            PB2: Pin::new(0x25, 0x24, 1 << 2),
+            PB3: Pin::new(0x25, 0x24, 1 << 3),
+            PB4: Pin::new(0x25, 0x24, 1 << 4),
+            PB5: Pin::new(0x25, 0x24, 1 << 5),
+            PB6: Pin::new(0x25, 0x24, 1 << 6),
+            PB7: Pin::new(0x25, 0x24, 1 << 7 ),
+
+            /// Port En
+            PC6: Pin::new(0x28, 0x27, 1 << 6),
+            PC7: Pin::new(0x28, 0x27, 1 << 7),
+
+            /// Port Dn
+            PD0: Pin::new(0x2B, 0x2A, 1 << 0),
+            PD1: Pin::new(0x2B, 0x2A, 1 << 1),
+            PD2: Pin::new(0x2B, 0x2A, 1 << 2),
+            PD3: Pin::new(0x2B, 0x2A, 1 << 3),
+            PD4: Pin::new(0x2B, 0x2A, 1 << 4),
+            PD5: Pin::new(0x2B, 0x2A, 1 << 5),
+            PD6: Pin::new(0x2B, 0x2A, 1 << 6),
+            PD7: Pin::new(0x2B, 0x2A, 1 << 7),
+
+            /// Port En
+            PE2: Pin::new(0x2E, 0x2D, 1 << 2),
+            PE6: Pin::new(0x2E, 0x2D, 1 << 6),
+
+            /// Port Fn
+            PF0: Pin::new(0x31, 0x30, 1 << 0),
+            PF1: Pin::new(0x31, 0x30, 1 << 1),
+            PF4: Pin::new(0x31, 0x30, 1 << 4),
+            PF5: Pin::new(0x31, 0x30, 1 << 5),
+            PF6: Pin::new(0x31, 0x30, 1 << 6),
+            PF7: Pin::new(0x31, 0x30, 1 << 7),
+        }
+    }
+
+    /// Busy-wait for `n` instruction
+    #[inline(always)]
+    fn busywait(&self, n: u16) {
+        unsafe {
+            asm!("1: sbiw {i}, 1",
+                 "brne 1b",
+                 i = inout(reg_iw) n => _,
+                 )
+        }
+    }
+}
+
 #[panic_handler]
 fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
     loop {}
@@ -14,48 +141,31 @@ fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
 extern "C" fn eh_personality() {
 }
 
-/// Busy-wait for `n` instruction
-#[inline(always)]
-fn busywait(n: u16) {
-    unsafe {
-        asm!("1: sbiw {i}, 1",
-             "brne 1b",
-             i = inout(reg_iw) n => _,
-             )
-    }
-}
 
 #[no_mangle]
-pub extern fn main() {
+pub unsafe extern fn main() {
 
-    // Magic values from datasheet
-    let port_b_adr = 0x25 as *const u8;
-    let ddr_b_adr  = 0x24 as *const u8;
-    let b5_mask: u8 = 1 << 5;
+    let mut atm = Atmega32p8::new();
 
-    unsafe {
+    atm.PB5.set_out();
+    atm.PB0.set_out();
+    atm.PD7.set_out();
 
-        // Set pin B5 to output
-        core::ptr::write_volatile(
-            ddr_b_adr as *mut u8, core::ptr::read_volatile(ddr_b_adr) | b5_mask);
+    loop {
+        atm.PB5.pin_on();
+        atm.PB0.pin_off();
+        atm.PD7.pin_on();
 
-        loop {
-            // Set pin B5 to 1
-            core::ptr::write_volatile(
-                port_b_adr as *mut u8, core::ptr::read_volatile(port_b_adr) | b5_mask);
+        for _ in 0..10  {
+            atm.busywait(65535);
+        }
 
-            for _ in 0..10  {
-                busywait(65535);
-            }
+        atm.PB5.pin_off();
+        atm.PB0.pin_on();
+        atm.PD7.pin_off();
 
-            // Set pin B5 to 0
-            core::ptr::write_volatile(
-                port_b_adr as *mut u8,
-                core::ptr::read_volatile(port_b_adr) & !b5_mask);
-
-            for _ in 0..10  {
-                busywait(65535);
-            }
+        for _ in 0..10  {
+            atm.busywait(65535);
         }
     }
 }
